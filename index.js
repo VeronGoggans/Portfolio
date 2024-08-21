4b81ce649c2c647b7249979de2647c7b0621014d
VeronGoggans <j.s.v.goggans@gmail.com>
2024-08-21 03:58:54 +0200

    Done with the about me view

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// import { templates } from "./javascript/templates.js";
+import { templates } from "./javascript/templates.js";
 
-// const header = document.querySelector('header')
-// const navbar = document.querySelector('nav');
-// const logo = header.querySelector('.logo');
-// const navLinks = navbar.querySelectorAll('a');
-// const main = document.querySelector('main');
-
-
-
-// // Adding eventlisteners
-// logo.addEventListener('click', () => {loadTemplate('home')});
-// navLinks.forEach(link => {
-//     link.addEventListener('click', (event) => {
-//         event.preventDefault();
-
-//         const anchor = event.target.closest('a[data-view]')
-//         if (anchor) {
-//             loadTemplate(
-//                 event.target, 
-//                 anchor.getAttribute('data-view')
-//             );
-//         }
-//     }
-// )})
+const header = document.querySelector('header')
+const navbar = document.querySelector('nav');
+const logo = header.querySelector('.logo');
+const navLinks = navbar.querySelectorAll('a');
+const main = document.querySelector('main');
 
 
 
-// function loadTemplate(navLink, templateName) {
-//     // Setting the template
-//     main.innerHTML = templates[templateName];
+// Adding eventlisteners
+logo.addEventListener('click', () => {loadTemplate('home')});
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
 
-//     // Updating the active tab style
-//     navLinks.forEach(nav => nav.classList.remove('active'));
+        const anchor = event.target.closest('a[data-view]')
+        if (anchor) {
+            loadTemplate(
+                event.target, 
+                anchor.getAttribute('data-view')
+            );
+        }
+    }
+)})
 
-//     // Add the active class to the clicked anchor tag
-//     navLink.classList.add('active')
-// }
 
-// main.innerHTML = templates['home']
+
+function loadTemplate(navLink, templateName) {
+    // Setting the template
+    main.innerHTML = templates[templateName];
+
+    // Updating the active tab style
+    navLinks.forEach(nav => nav.classList.remove('active'));
+
+    // Add the active class to the clicked anchor tag
+    navLink.classList.add('active')
+}
+
+main.innerHTML = templates['home']
--- javascript/templates.js
+++ javascript/templates.js
@@ -29,8 +29,7 @@ export const aboutTemplate = `
                         <div class="text-box">
                             <h2>Python</h2>
                             <small>2022 - Now</small>
-                            <p>Like most I Started learning Python in my first year of school,
-                            to understand the basics of programming.</p>
+                            <p>Like many others, I began learning Python during my first year of school to grasp the fundamentals of programming.</p>
                             <span class="left-container-arrow"></span>
                         </div>
                     </div>
@@ -40,8 +39,7 @@ export const aboutTemplate = `
                         <div class="text-box">
                             <h2>Java</h2>
                             <small>2022 - Now</small>
-                            <p>After understanding the basics of programming, I started learning Java.
-                                And this is the language my school will be teaching us for the rest of the study</p>
+                            <p>After mastering the basics of programming, I transitioned to learning Java, which introduced me to the object-oriented programming (OOP) paradigm. This is also the language my school will focus on for the remainder of my studies.</p>
                             <span class="right-container-arrow"></span>
                         </div>
                     </div>
@@ -51,8 +49,7 @@ export const aboutTemplate = `
                         <div class="text-box">
                             <h2>Springboot</h2>
                             <small>2022 - Now</small>
-                            <p>After understanding the basics of programming, I started learning Java.
-                                And this is the language my school will be teaching us for the rest of the study</p>
+                            <p>I used Java to develop APIs, leveraging the popular Spring Boot framework to efficiently handle this task. Spring Boot's robust features and ease of use made it an ideal choice for building scalable and maintainable APIs.</p>
                             <span class="left-container-arrow"></span>
                         </div>
                     </div>
@@ -62,8 +59,7 @@ export const aboutTemplate = `
                         <div class="text-box">
                             <h2>Postman</h2>
                             <small>2022 - Now</small>
-                            <p>After understanding the basics of programming, I started learning Java.
-                                And this is the language my school will be teaching us for the rest of the study</p>
+                            <p>When developing APIs, testing the endpoints is a crucial aspect of the process. For this purpose, I rely on Postman, which allows me to efficiently test and debug API endpoints to ensure they function as expected.</p>
                             <span class="right-container-arrow"></span>
                         </div>
                     </div>
@@ -73,41 +69,65 @@ export const aboutTemplate = `
                         <div class="text-box">
                             <h2>Posgressql</h2>
                             <small>2022 - Now</small>
-                            <p>After understanding the basics of programming, I started learning Java.
-                                And this is the language my school will be teaching us for the rest of the study</p>
+                            <p>As a backend developer, having a solid understanding of databases is essential. PostgreSQL was the first database I learned, starting with basic queries and progressing to more complex ones</p>
                             <span class="left-container-arrow"></span>
                         </div>
                     </div>
 
                     <div class="container right-container">
-                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg">
+                        <img src="https://camo.githubusercontent.com/fcafa5ebc1f5f789ae7d012a3ecd8fe7bda49516591caf7c37698f764165d880/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667">
                         <div class="text-box">
-                            <h2>HTML</h2>
+                            <h2>Git</h2>
                             <small>2022 - Now</small>
-                            <p>Like most I Started learning Python in my first year of school,
-                            to understand the basics of programming.</p>
+                            <p>Throughout all my school and personal projects, I rely on Git for version control to manage changes and keep my code securely stored. Git has been essential in tracking progress, collaborating with others, and ensuring the safety of my work.</p>
                             <span class="right-container-arrow"></span>
                         </div>
                     </div>
 
                     <div class="container left-container">
-                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg">
+                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg">
                         <div class="text-box">
-                            <h2>CSS</h2>
+                            <h2>HTML</h2>
                             <small>2022 - Now</small>
-                            <p>Like most I Started learning Python in my first year of school,
-                            to understand the basics of programming.</p>
+                            <p> HTML was introduced in both my first and second years of school, 
+                                though it was never the main focus. I truly became comfortable with HTML while developing my note-taking app, Neutron.
+                                 </p>
                             <span class="left-container-arrow"></span>
                         </div>
                     </div>
 
                     <div class="container right-container">
+                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg">
+                        <div class="text-box">
+                            <h2>CSS</h2>
+                            <small>2022 - Now</small>
+                            <p>CSS was also not a major focus in school, so I became comfortable with it primarily during the development of Neutron. This project provided the practical experience I needed to enhance my CSS skills.</p>
+                            <span class="right-container-arrow"></span>
+                        </div>
+                    </div>
+
+                    <div class="container left-container">
                         <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg">
                         <div class="text-box">
                             <h2>Javascript</h2>
                             <small>2022 - Now</small>
-                            <p>Like most I Started learning Python in my first year of school,
-                            to understand the basics of programming.</p>
+                            <p>Javascript is another language that I got a good grasp on during development of Neutron. <br><br>
+                                I learned frondend paradigms like 
+                                <ul>
+                                    <li>MVC (Modal, View, Controller)</li>
+                                    <li>SPA (Single Page Application)</li>
+                                </ul>
+                            </p>
+                            <span class="left-container-arrow"></span>
+                        </div>
+                    </div>
+
+                    <div class="milestone right-container">
+                        <div class="dot"></div>
+                        <div class="text-box">
+                            <h2>Milestone</h2>
+                            <small>2022 - 2023</small>
+                            <h2 class="title">End of School year 1</h2>
                             <span class="right-container-arrow"></span>
                         </div>
                     </div>
@@ -116,9 +136,10 @@ export const aboutTemplate = `
                         <img src="https://camo.githubusercontent.com/f32e9cca1f0df0138a8f536217daa54ad21b6913642422f32e3c5c623f3a06b9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667">
                         <div class="text-box">
                             <h2>Figma</h2>
-                            <small>2022 - Now</small>
-                            <p>Like most I Started learning Python in my first year of school,
-                            to understand the basics of programming.</p>
+                            <small>2023 - Now</small>
+                            <p>I began using Figma when I worked on a team project for a company called KPN. 
+                                We used Figma to design the frontend and collaborate effectively with the team, allowing us to share ideas, iterate on designs, and ensure a cohesive user experience.
+                            </p>
                             <span class="left-container-arrow"></span>
                         </div>
                     </div>
@@ -127,27 +148,34 @@ export const aboutTemplate = `
                         <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg">
                         <div class="text-box">
                             <h2>MongoDB</h2>
-                            <small>2022 - Now</small>
-                            <p>Like most I Started learning Python in my first year of school,
-                            to understand the basics of programming.</p>
+                            <small>2023 - Now</small>
+                            <p>MongoDB, a NO-SQL database, was the second database I had to learn, during a project while working for a company called HydroNet</p>
                             <span class="right-container-arrow"></span>
                         </div>
                     </div>
 
-                    <div class="container left-container">
-                        <img src="https://camo.githubusercontent.com/fcafa5ebc1f5f789ae7d012a3ecd8fe7bda49516591caf7c37698f764165d880/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667"">
+                    <div class="milestone left-container">
+                        <div class="dot"></div>
                         <div class="text-box">
-                            <h2>Git</h2>
-                            <small>2022 - Now</small>
-                            <p>Like most I Started learning Python in my first year of school,
-                            to understand the basics of programming.</p>
+                            <h2>Milestone</h2>
+                            <small>2023 - 2024</small>
+                            <h2 class="title">End of School year 2</h2>
                             <span class="left-container-arrow"></span>
                         </div>
                     </div>
 
-                    
+                    <div class="milestone right-container">
+                        <div class="dot"></div>
+                        <div class="text-box">
+                            <h2>Milestone</h2>
+                            <small>Summer of 2024</small>
+                            <h2 class="title">Neutron 1.0 Release</h2>
+                            <span class="right-container-arrow"></span>
+                        </div>
+                    </div>
+
                 </div>
-                <h3 class="skills-heading">My <span>Skills</span></h3>
+                <h3 class="skills-heading">Programming <span>Languages</span></h3>
     
                 <table>
                     <tr>
@@ -155,26 +183,37 @@ export const aboutTemplate = `
                         <th>Experience</th>
                     </tr>
                     <tr>
-                        <td><i class="fa-brands fa-python"></i> Python</td>
+                        <td>Python</td>
                         <td>2 years</td>
                     </tr>
                     <tr>
-                        <td><i class="fa-brands fa-java"></i> Java</td>
+                        <td>Java</td>
                         <td>1.5 years</td>
                     </tr>
                     <tr>
-                        <td><i class="fa-brands fa-js"></i> Javascript</td>
+                        <td>Javascript</td>
                         <td>1.5 years</td>
                     </tr>
                     <tr>
-                        <td><i class="fa-brands fa-html5"></i> HTML</td>
+                        <td>HTML</td>
                         <td>2 years</td>
                     </tr>
                     <tr>
-                        <td><i class="fa-brands fa-css3-alt"></i> CSS</td>
+                        <td>CSS</td>
                         <td>2 years</td>
                     </tr>
                 </table>
+
+                <h3 class="experience-heading">Experience working <span>with</span></h3>
+
+                <div class="experience-container">
+                    <img src="https://camo.githubusercontent.com/491e3e316785d254f8709adfeb919a68582e0bef2946e15195e1f66de5e98b10/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f737072696e67696f2f737072696e67696f2d69636f6e2e737667">
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg">
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg">
+                    <img src="https://camo.githubusercontent.com/f32e9cca1f0df0138a8f536217daa54ad21b6913642422f32e3c5c623f3a06b9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667">
+                    <img src="https://camo.githubusercontent.com/a13ca5b988ada41839ebe4f88455e63419a1b56fcb5eda207794cd1649a61d2c/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667">
+                    <img src="https://camo.githubusercontent.com/fcafa5ebc1f5f789ae7d012a3ecd8fe7bda49516591caf7c37698f764165d880/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667"">
+                </div>
             </div>
         </section>
 
